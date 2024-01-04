7df350296a23dd5c7133ef6a47d17679f128b8d4
FranC-prog <franciscocalveyra24@gmail.com>
2024-01-04 11:26:29 -0300

    base-struct minor changes, testing node recolor  on-click

--- src/components/Nodee.js
+++ src/components/Nodee.js
@@ -1,8 +1,20 @@
 import React from "react";
 
+function applyStyles(){
+    setStyle({
+        backgroundColor: "green"
+    });
+}
+
 export const Nodee = ({nodeId, name, onClick}) => {
+
+    const handleClick = (event) => {
+        applyStyles();
+        onClick(event);
+    }
+
     return (
-        <button id={nodeId} onClick={onClick} >
+        <button id={nodeId} onClick={handleClick}>
             {name}
         </button>
     )
