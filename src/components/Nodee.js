9c8e8198a0293e2c53c819f7024d8db14b96934a
FranC-prog <franciscocalveyra24@gmail.com>
2024-01-16 10:12:17 -0300

    Refactoring, nodes are now not needed for rendering

--- src/components/Nodee.js
+++ src/components/Nodee.js
@@ -1,51 +1,51 @@
 import React from "react";
-import {useState} from "react";
+import {useState, useRef} from "react";
 import Navbar from "./Navbar"
 export const Nodee = ({nodeId, name, type}) => {
     const [clicked, setClick] = useState(false);
-    const nodes = document.getElementById(nodeId)
-    function changeColour(){
-        const colour = '#dedede';
-        const other_colour = 'lightblue'
+    const nodeRef = useRef(null);
+    function changeColor(){
+        const course_color = '#dedede';
+        const other_color = 'darksalmon'
         if(type === 'course'){
             if(clicked) {
-                nodes.style.background = 'dimgray';
+                nodeRef.current.style.background = 'dimgray';
                 setClick(false);
-                nodes.onmouseover = function (){
-                    nodes.style.background = 'dimgray';
-                    if(nodes.style.background === 'dimgray') nodes.style.color = colour;
-                    else nodes.style.color = 'black'
+                nodeRef.current.onmouseover = function (){
+                    nodeRef.current.style.background = 'dimgray';
+                    if(nodeRef.current.style.background === 'dimgray') nodeRef.current.style.color = course_color;
+                    else nodeRef.current.style.color = 'black'
 
                 }
-                nodes.onmouseleave = function (){
-                    nodes.style.background = colour;
-                    nodes.style.color = 'black';
+                nodeRef.current.onmouseleave = function (){
+                    nodeRef.current.style.background = course_color;
+                    nodeRef.current.style.color = 'black';
                 }
 
             }
             else{
-                nodes.style.background = 'green';
+                nodeRef.current.style.background = 'green';
                 setClick(true);
-                nodes.onmouseover = function (){
-                    nodes.style.color = colour;
+                nodeRef.current.onmouseover = function (){
+                    nodeRef.current.style.color = course_color;
                 }
-                nodes.onmouseleave = function (){
-                    nodes.style.color = 'black';
+                nodeRef.current.onmouseleave = function (){
+                    nodeRef.current.style.color = 'black';
                 }
 
             }
         }
         else{
-            nodes.style.background = other_colour;
+            nodeRef.current.style.background = other_color;
         }
     }
     function handleClick(){
-        changeColour();
+        changeColor();
         //Here should go the Navbar() function but it doesn't work for now
     }
 
     return (
-        <button id={nodeId} onClick={handleClick} className='course-button' type={type}>
+        <button id={nodeId} onClick={handleClick} className='course-button' type={type} ref={nodeRef}>
             {name}
         </button>
     )
