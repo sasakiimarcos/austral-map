e711ff5e3d5d15f84c5357a1352eaabceb6c8099
Marcos Sasaki <sasakimarcos95@gmail.com>
2024-01-18 22:46:05 -0300

    Created separate functions for hover effect

--- src/components/Nodee.js
+++ src/components/Nodee.js
@@ -4,44 +4,14 @@ export const Nodee = ({nodeId, name, type, hoveredNode, setHoveredNode}) => {
     const [clicked, setClick] = useState(false);
     const nodeRef = useRef(null);
     function changeColor(){
-        
-        const course_color = '#dedede';
         if(type === 'course'){
             if(clicked) {
                 nodeRef.current.style.background = 'dimgray';
                 setClick(false);
-                nodeRef.current.onmouseover = function (){
-                    setHoveredNode(nodeId);
-
-                    nodeRef.current.style.background = 'dimgray';
-                    if(nodeRef.current.style.background === 'dimgray') nodeRef.current.style.color = course_color;
-                    else nodeRef.current.style.color = 'black'
-
-                }
-                nodeRef.current.onmouseleave = function (){
-                    // Now that no node is hovered, return to full opacity
-                    setHoveredNode(null);
-
-                    nodeRef.current.style.background = course_color;
-                    nodeRef.current.style.color = 'black';
-                }
-
             }
             else{
                 nodeRef.current.style.background = 'green';
                 setClick(true);
-                nodeRef.current.onmouseover = function (){
-                    setHoveredNode(nodeId);
-
-                    nodeRef.current.style.color = course_color;
-                }
-                nodeRef.current.onmouseleave = function (){
-                    // Now that no node is hovered, return to full opacity
-                    setHoveredNode(null);
-
-                    nodeRef.current.style.color = 'black';
-                }
-
             }
         }
     }
@@ -50,12 +20,45 @@ export const Nodee = ({nodeId, name, type, hoveredNode, setHoveredNode}) => {
         //Here should go the Navbar() function but it doesn't work for now
     }
 
+    function enterNode() {
+        if(clicked) {
+            nodeRef.current.style.background = 'dimgray';
+            if(nodeRef.current.style.background === 'dimgray') nodeRef.current.style.color = '#dedede';
+            else nodeRef.current.style.color = 'black'
+
+        }
+        else{
+            nodeRef.current.style.color = '#dedede';
+            nodeRef.current.style.background = 'dimgray'
+
+        }
+    }
+
+    function leaveNode() {
+        if(clicked) {
+            // Now that no node is hovered, return to full opacity
+            nodeRef.current.style.background = '#dedede';
+            nodeRef.current.style.color = 'black';
+        }
+        else{
+            // Now that no node is hovered, return to full opacity
+            nodeRef.current.style.background = '#dedede';
+            nodeRef.current.style.color = 'black';
+        }
+    }
+
     return (
         <button 
             id={nodeId}
             onDoubleClick={handleClick}
-            onMouseEnter={() => {setHoveredNode(nodeId)}}
-            onMouseLeave={() => {setHoveredNode(null)}}
+            onMouseEnter={() => {
+                setHoveredNode(nodeId)
+                enterNode()
+            }}
+            onMouseLeave={() => {
+                setHoveredNode(null)
+                leaveNode()
+            }}
             className='course-button'
             type={type}
             ref={nodeRef}
